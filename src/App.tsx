3c4c7dd39828bb9a2da93dcb5328a77481ad7b0a
Denis <denisskulakov@gmail.com>
2024-04-15 22:26:35 +0300

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import WebApp from '@twa-dev/sdk'
+import WebApp from '@twa-dev/sdk' 
 
 function App() {
   const [count, setCount] = useState(0)
